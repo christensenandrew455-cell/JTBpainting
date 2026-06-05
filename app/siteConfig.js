cf740c86b7332c1b819baa2b1bf37749d684a3af
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-06-05 11:39:21 -0400

    Create siteConfig.js

---
+++ app/siteConfig.js
@@ -0,0 +1,16 @@
+const siteConfig = {
+// Header
+showHeader: true,
+stickyHeader: false,
+
+// Footer
+showFooter: true,
+footerCentered: true,
+
+// Layout
+contentAlignment: "left", // left | center
+pagePadding: "p-6 md:p-12",
+maxWidth: "max-w-7xl",
+};
+
+export default siteConfig;
