aa192c9791208724c147c130d32a76460e03a4f5
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-06-05 16:19:43 -0400

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,6 +1,5 @@
 import { Resend } from "resend";
-import emailConfig from "@/app/contact/emailConfig";
-
+import emailConfig from "../../contact/emailConfig";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
